0f9e61d83b08cbb6ea939f0bddaa562abc5d4d39
Larissa Freire <48482909+LariFreire@users.noreply.github.com>
2025-07-07 22:21:49 -0300

    Update 1.13.0_phone.sql

--- migrations-semver/1.13.0_phone.sql
+++ migrations-semver/1.13.0_phone.sql
@@ -1,3 +1,3 @@
 ALTER TABLE IF EXISTS users ADD COLUMN IF NOT EXISTS phone VARCHAR(255) NOT NULL DEFAULT '';
 
-INSERT INTO users (id, name) VALUES (1, 'João');
+INSERT INTO users (id, name) VALUES (1, 'João', 'joao@hotmail.com');
